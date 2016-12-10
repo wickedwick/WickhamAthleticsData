a7a091fead0a3dd0d2b9e918cfedb8950adc94a0
Travis Wickham <twickham37@gmail.com>
2016-12-10 16:27:24 -0600

    Added Staff table. Updates to DataAccess Items and the model.

---
+++ WickhamAthleticsData/DataAccess/Layout/ContentDataAccess.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WickhamAthleticsData.Model;
+using WickhamAthleticsData.DataAccess.Base;
+
+namespace WickhamAthleticsData.DataAccess.Layout
+{
+    public class ContentDataAccess
+    {
+        public static Content GetContentById(int id)
+        {
+            Content content = null;
+
+            using (WickhamAthleticsEntities ctx = new WickhamAthleticsEntities())
+            {
+                try
+                {
+                    content = ctx.Content.Where(c => c.PK_ContentID == id).FirstOrDefault();
+                }
+                catch (Exception ex)
+                {
+                    //
+                }
+            }
+
+            return content;
+        }
+
+        public static Content GetContentByName(string name)
+        {
+            Content content = null;
+
+            using (WickhamAthleticsEntities ctx = new WickhamAthleticsEntities())
+            {
+                try
+                {
+                    content = ctx.Content.Where(c => c.Name.ToLower().Equals(name.ToLower())).FirstOrDefault();
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+
+            return content;
+        }
+    }
+}
--- WickhamAthleticsData/DataAccess/Layout/StaffDataAccess.cs
+++ WickhamAthleticsData/DataAccess/Layout/StaffDataAccess.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WickhamAthleticsData
+namespace WickhamAthleticsData.DataAccess.Layout
 {
-    public class Class1
+    public class StaffDataAccess
     {
     }
 }
